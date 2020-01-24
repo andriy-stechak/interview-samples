21b3acd627fc660cee0f3d07f20997b7283a4cf6
Andriy Stechak <andriy.stechak@gmail.com>
2020-01-24 14:19:36 +0200

    Remove confusing `then` statement

--- promises.js
+++ promises.js
@@ -1,7 +1,7 @@
 setTimeout(console.log, 0, 1)
 const p1 = Promise.resolve(2).then(console.log);
 setTimeout(console.log, 0, 3)
-const p2 = Promise.resolve(4).then(console.log).then(p1);
+const p2 = Promise.resolve(4).then(console.log);
 setImmediate(() => console.log(5))
 process.nextTick(() => console.log(6))
 Promise.all([p1, p2]);
